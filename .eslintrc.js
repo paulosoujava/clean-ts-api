7b37a9a188f228e26b3875b7d04debb68d1bf637
Paulo jorge <paulosoujava@gmail.com>
2020-07-30 15:03:23 -0300

    chore: update json standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,13 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/consistent-type-definitions': 'off'
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/comma-spacing': 'off',
+    // caso nao queira usar o pdarao do esllint descomente abaixo
+    // padrao eslint:
+    // decrypt: (value: string) => Promisse<string>
+    // '@typescript-eslint/method-signature-style': 'off'
+    '@typescript-eslint/return-await': 'off'
 
   }
 }
