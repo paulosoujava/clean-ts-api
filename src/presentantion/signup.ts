11de7522b6fc5500c9094a2cf743a91c895667b8
Paulo jorge <paulosoujava@gmail.com>
2020-07-30 15:51:47 -0300

    refactor: ensure signupcontorller interface

--- src/presentantion/signup.ts
+++ src/presentantion/signup.ts
@@ -1,5 +1,7 @@
+import { HttpRequest, HttpResponse } from './protocols/http'
+
 export class SignUpController {
-  handler (httpRequest: any): any {
+  handler (httpRequest: HttpRequest): HttpResponse {
     const { name, email } = httpRequest.body
     if (!name) {
       return {
