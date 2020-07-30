d6d03f92793a439f6b81e65111aa983af9730f8a
Paulo jorge <paulosoujava@gmail.com>
2020-07-30 16:05:14 -0300

    refactor: signup fields

--- src/presentantion/signup.ts
+++ src/presentantion/signup.ts
@@ -4,12 +4,9 @@ import { badRequest } from './helpers/http-helpers'
 
 export class SignUpController {
   handler (httpRequest: HttpRequest): HttpResponse {
-    const { name, email } = httpRequest.body
-    if (!name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) { return badRequest(new MissingParamError(field)) }
     }
   }
 }
