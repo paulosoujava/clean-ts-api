3435e3b64ee83d13bdde4ae819d7ed75e668603b
Paulo jorge <paulosoujava@gmail.com>
2020-07-30 17:37:16 -0300

    feat: validate password

--- src/presentantion/signup.spec.ts
+++ src/presentantion/signup.spec.ts
@@ -71,6 +71,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
+  test('should return 400 if no password confirmation fails', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.handler(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
   test('should return 400 if no passwordConfirmation is provided', () => {
     const { sut } = makeSut()
     const httpRequest = {
--- src/presentantion/signup.ts
+++ src/presentantion/signup.ts
@@ -16,6 +16,8 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
+      const { password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) { return badRequest(new InvalidParamError('passwordConfirmation')) }
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) { return badRequest(new InvalidParamError('email')) }
     } catch (error) {
