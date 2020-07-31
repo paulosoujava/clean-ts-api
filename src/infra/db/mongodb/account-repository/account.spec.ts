adaff8de287f19e417e1ae101bd00f37d2de43e0
Paulo jorge <paulosoujava@gmail.com>
2020-07-31 18:13:47 -0300

    test: add delete many in account

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,5 +1,10 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+
+const makeSut = (): AddAccountRepository => {
+  return new AccountMongoRepository()
+}
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -10,8 +15,13 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@email.com',
